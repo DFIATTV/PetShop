59e92cf2e7cf4db9a5fdf9529aa5782759c39176
Admin <dfiattv@ucenfotec.ac.cr>
2023-04-04 19:00:59 -0600

    ignoring nodemodules

--- src/scenes/juguetes/Index.jsx
+++ src/scenes/juguetes/Index.jsx
@@ -4,9 +4,6 @@ import { Box, /*Typography, useTheme*/ } from "@mui/material";
 import Header from "../../components/Header";
 
 const Juguetes = () => {
-  //const theme = useTheme();
-  //const colors = tokens(theme.palette.mode);
-  //Dentro del box vamos a cargar solo los items con el tag de juguete
   return (
     <Box>
       <Header title="Juguetes" />
